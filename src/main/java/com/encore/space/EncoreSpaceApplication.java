8cf2333b9d6877ba6d33f487549778a99bde7de2
sejongPark <lifedesigner88@gmail.com>
2024-01-28 10:50:26 +0900

    first commit

---
+++ src/main/java/com/encore/space/EncoreSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.encore.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncoreSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncoreSpaceApplication.class, args);
+    }
+
+}
