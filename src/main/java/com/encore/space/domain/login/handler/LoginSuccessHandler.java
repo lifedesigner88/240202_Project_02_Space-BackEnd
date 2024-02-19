348d8529c42c1d98e1cbcadd4f3fe639e49a6135
GyeongNam <jo_gn97@naver.com>
2024-02-20 00:40:04 +0900

    feat : oauth2 로그인 관련 수정
    1. 클라이언트 8081로 진입하는것이 아니라 서버 8080으로 get 방식으로 진입
    2. 모든 인증절차를 마무리
    3. LoginSuccessHandler 나 LoginFailureHandler 를 이용해 클라이언트로 (8081) 에 쿠키에 jwt를 넣어서 보냄

--- src/main/java/com/encore/space/domain/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/encore/space/domain/login/handler/LoginSuccessHandler.java
@@ -6,17 +6,20 @@ import com.encore.space.domain.login.jwt.JwtProvider;
 import com.encore.space.domain.login.service.LoginService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -47,21 +50,20 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
-
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-        objectMapper.writeValue(
-                response.getWriter(),
-                CommonResponse.builder()
-                        .httpStatus(HttpStatus.OK)
-                        .message("로그인 되었습니다.")
-                        .result(
-                                jwtProvider.exportToken(
-                                        oauth2User.getAttribute("email"),
-                                        Objects.requireNonNull(oauth2User.getAttribute("role")).toString(),
-                                        webConfig.ipCheck(request)
-                                )
-                        )
-                        .build()
-        );
+
+
+        Cookie cookie = new Cookie("accessToken", jwtProvider.exportToken(
+                oauth2User.getAttribute("email"),
+                Objects.requireNonNull(oauth2User.getAttribute("role")).toString(),
+                webConfig.ipCheck(request)));
+        cookie.setPath("/"); // 모든 경로에 대해 쿠키 전송
+        response.addCookie(cookie);
+
+        String redirectUrl = "http://localhost:8081/oauth2/redirect";
+
+        // 클라이언트로 리다이렉트
+        response.setStatus(HttpStatus.FOUND.value());
+        response.setHeader(HttpHeaders.LOCATION, redirectUrl);
     }
 }
